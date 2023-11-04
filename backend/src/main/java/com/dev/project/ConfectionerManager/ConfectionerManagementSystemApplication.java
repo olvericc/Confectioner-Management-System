88795cc7185963de4b450c9acc78d0766c80113a
olvericc <olv.ericlopes@gmail.com>
2023-11-04 16:38:23 -0300

    project: creating the backend with spring boot structure

---
+++ backend/src/main/java/com/dev/project/ConfectionerManager/ConfectionerManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package com.dev.project.ConfectionerManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfectionerManagementSystemApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ConfectionerManagementSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dev/project/ConfectionerManager/ConfectionerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.project.ConfectionerManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfectionerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
