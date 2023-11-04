8aac4773ab7a0c2df68e6b95e6d4e250a83d261f
olvericc <olv.ericlopes@gmail.com>
2023-11-04 17:28:03 -0300

    feat: implements the User class and table in database

---
+++ backend/src/main/java/com/dev/project/ConfectionerManager/POJO/User.java
@@ -0,0 +1,43 @@
+package com.dev.project.ConfectionerManager.POJO;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@Entity
+@DynamicUpdate
+@DynamicInsert
+@Table(name = "users")
+public class User implements Serializable
+{
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "contact_number")
+    private String contactNumber;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "role")
+    private String role;
+}
